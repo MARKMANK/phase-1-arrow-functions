de395e79c9b5cacc10cce54d3f52bbcc80450bd9
kevinmarkman <kevin.w.markman@gmail.com>
2023-02-24 17:52:07 -0600

    Adds function divide, arrow funtion square, and arrow function add

---
+++ index.js
@@ -0,0 +1,11 @@
+const firstNumber = 2000;
+const secondNumber = 100;
+
+
+function divide (firstNumber , secondNumber){
+    return firstNumber / secondNumber;
+}
+
+const square = numberParameter => numberParameter ** 2;
+
+const add = (firstNumber , secondNumber) => firstNumber + secondNumber;
